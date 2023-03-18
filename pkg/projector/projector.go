6fff34f8fdf8d06da232514de21a00411e16424e
mrohner94 <87048324+mrohner94@users.noreply.github.com>
2023-03-18 16:42:42 -0400

    fin

--- pkg/projector/projector.go
+++ pkg/projector/projector.go
@@ -24,51 +24,53 @@ func CreateProjector(config *Config, data *Data) *Projector {
 
 func (p *Projector) GetValue(key string) (string, bool) {
 
-	curr := p.config.Pwd
-	prev := ""
+    curr := p.config.Pwd
+    prev := ""
 
-	out := ""
-	found := false
-	for curr != prev {
+    out := ""
+    found := false
+    for ; curr != prev; {
 
-		if dir, ok := p.data.Projector[curr]; ok {
-			if value, ok := dir[key]; ok {
-				out = value
-				found = true
-				break
-			}
-		}
+        if dir, ok := p.data.Projector[curr]; ok {
+            if value, ok := dir[key]; ok {
+                out = value
+                found = true
+                break
+            }
+        }
 
-		prev = curr
-		curr = path.Dir(curr)
-	}
-	return out, found
+        prev = curr
+        curr = path.Dir(curr)
+    }
+
+    return out, found
 }
 
+
 func (p *Projector) GetValueAll() map[string]string {
-	out := map[string]string{}
+    out := map[string]string{}
 
-	paths := []string{}
+    paths := []string{}
+    curr := p.config.Pwd
+    prev := ""
 
-	curr := p.config.Pwd
-	prev := ""
+    for ; curr != prev; {
+        paths = append(paths, curr)
+        prev = curr
+        curr = path.Dir(curr)
+    }
 
-	for curr != prev {
-		paths = append(paths, curr)
-		prev = curr
-		curr = path.Dir(curr)
-	}
+    for i := len(paths) - 1; i >= 0; i-- {
 
-	for i := len(paths) - 1; i >= 0; i-- {
+        if dir, ok := p.data.Projector[paths[i]]; ok {
+            for k, v := range dir {
+                out[k] = v
+            }
+        }
 
-		if dir, ok := p.data.Projector[paths[i]]; ok {
-			for k, v := range dir {
-				out[k] = v
-			}
-		}
-	}
+    }
 
-	return out
+    return out
 }
 
 func (p *Projector) SetValue(key, value string) {
@@ -85,6 +87,23 @@ func (p *Projector) RemoveValue(key string) {
 	if dir, ok := p.data.Projector[p.config.Pwd]; ok {
 		delete(dir, key)
 	}
+}
+
+func (p *Projector) Save() error {
+	dir := path.Dir(p.config.Config)
+
+	if _, err := os.Stat(dir); os.IsNotExist(err) {
+		err = os.MkdirAll(dir, 0755)
+		if err != nil {
+			return err
+		}
+	}
+	jsonString, err := json.Marshal(p.data)
+	if err != nil {
+		return err
+	}
+	os.WriteFile(p.config.Config, jsonString, 0755)
+	return nil
 }
 
 func defaultProjector(config *Config) *Projector {
